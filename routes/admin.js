ff7c6dd0505dfd45305b76b37934da29ff2eb3bb
Glory Ozonuwe <ozo18001@byui.edu>
2022-01-15 23:42:44 -0700

    updates

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,7 @@ router.get('/add-product', (req, res, next) => {
 
 // this changes the URL to /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title });
+    products.push({ title: req.body.title, price: req.body.price, description: req.body.description});
     res.redirect('/');
 });
   
