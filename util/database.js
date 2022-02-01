7508b0b2d2b633d1e4509726f2aa3f61070803d0
Glory Ozonuwe <ozo18001@byui.edu>
2022-02-01 01:32:36 -0700

    MongoDB & Mongoose Update

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'cowbell0MILK', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
