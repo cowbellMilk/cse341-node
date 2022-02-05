c3ee8aa340894f6eef93b5595125c6e74ac3558d
Glory Ozonuwe <ozo18001@byui.edu>
2022-02-05 01:04:26 -0700

    week 5 commit

--- app.js
+++ app.js
@@ -3,26 +3,50 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://admin-user:12345@cse341cluster.lkzbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+const MONGODB_URI = 'mongodb+srv://admin-user:12345@cse341cluster.lkzbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById('61f601b9c48212549fedc381')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -30,28 +54,36 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
+
+const corsOptions = {
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  family: 4
+};
+
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://admin-user:12345@cse341cluster.lkzbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
 mongoose
-  .connect(
-    MONGODB_URL
-  )
+  .connect(MONGODB_URL, options)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(PORT);
   })
   .catch(err => {
